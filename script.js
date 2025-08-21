bc47c021b5ee72bec120b20006aba038e36d52ed
tonyt243 <quochuyta243@gmail.com>
2025-08-20 19:56:48 -0700

    Add image slide in frontpage

--- script.js
+++ script.js
@@ -142,6 +142,24 @@ function filterCategory() {
   galleries.forEach(gallery => sortProductsByName(gallery));
 }
 
+//Frontpage image slides
+let slideIndex = 0;
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }    
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 4000); 
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
+
+
+
 
 
 
